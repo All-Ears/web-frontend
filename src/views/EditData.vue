437afb1fddbd8fac1e3327cfaf86c36422fecfb6
Jared Weinberger <jared.j.weinberger@gmail.com>
2021-03-02 09:00:05 +0000

    removed typescript errors

--- src/views/EditData.vue
+++ src/views/EditData.vue
@@ -6,7 +6,7 @@ div(class="p-4")
     div(class="h-20")
     div(class="fixed bottom-0 h-20 w-screen flex flex-row justify-between items-center p-5 border-t-2 bg-white"
         v-if="loadState !== 'done'")
-        button(class="rounded bg-green-300 py-1 px-3" @click="addEmptyRecord()")
+        button(class="rounded bg-green-300 py-1 px-3" @click="addAndScrollToBottom()")
             font-awesome-icon(class="mr-2" icon="plus")
             | Add Record
         button(class="rounded bg-green-300 py-1 px-3" @click="saveChanges()")
@@ -15,12 +15,12 @@ div(class="p-4")
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref, useContext } from "vue"
+import { defineComponent, onMounted, ref } from "vue"
 import MikeRecordTable from "@/components/MikeRecordTable.vue"
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { MikeRecord, ProcessState, getMikePrimaryKey } from "@/models"
 import Axios, { AxiosResponse } from "axios"
-import Lodash, { last } from "lodash"
+import Lodash from "lodash"
 import Router from "@/router"
 
 export default defineComponent({
@@ -51,7 +51,7 @@ export default defineComponent({
                 })
         }
 
-        function addAndScrollToBottom(event: MouseEvent) {
+        function addAndScrollToBottom() {
             records.value.push({
                 unRegion: "",
                 subregionName: "",
@@ -90,16 +90,18 @@ export default defineComponent({
 
         function saveChanges() {
             submissionState.value = "waiting"
-            const [validRecords, _] = Lodash.partition(
+            const validRecords = Lodash.partition(
                 records.value,
                 recordIsValid
-            )
+            )[0]
             // make sets for O(1) access
-            let originalKeySet = new Set(originalRecords.map(getMikePrimaryKey))
-            let userKeySet = new Set(validRecords.map(getMikePrimaryKey))
+            const originalKeySet = new Set(
+                originalRecords.map(getMikePrimaryKey)
+            )
+            const userKeySet = new Set(validRecords.map(getMikePrimaryKey))
 
             // records with primary keys that are not in the originals
-            let [added, changedAndUnchanged] = Lodash.partition(
+            const [added, changedAndUnchanged] = Lodash.partition(
                 validRecords,
                 ({ mikeSiteId, year }) =>
                     !originalKeySet.has({ mikeSiteId, year })
@@ -107,10 +109,12 @@ export default defineComponent({
 
             const originalSet = new Set(originalRecords)
             // user records with the same primary keys as the originals that are otherwise changed
-            let changed = changedAndUnchanged.filter((x) => originalSet.has(x))
+            const changed = changedAndUnchanged.filter((x) =>
+                originalSet.has(x)
+            )
 
             // records whose primary keys are in the original copy but not the user one
-            let removed = originalRecords.filter(({ mikeSiteId, year }) =>
+            const removed = originalRecords.filter(({ mikeSiteId, year }) =>
                 userKeySet.has({ mikeSiteId, year })
             )
 
